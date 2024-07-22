f3b7489201385db23141cdc59dc9c406f13fb42b
Rust Lyu <778324184@qq.com>
2024-07-22 11:07:04 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,72 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+
+#include "decode.h"
+#include "encode.h"
+#include "player.h"
+#include "playaudio.h"
 
 extern "C" {
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
 }
 
-void print_video_info(const std::string& filename) {
-    // 初始化FFmpeg库
-    avformat_network_init();
-
-    // 打开视频文件
-    AVFormatContext* formatContext = avformat_alloc_context();
-    if (avformat_open_input(&formatContext, filename.c_str(), nullptr, nullptr) != 0) {
-        std::cerr << "Could not open file: " << filename << std::endl;
-        return;
-    }
-
-    // 读取视频文件信息
-    if (avformat_find_stream_info(formatContext, nullptr) < 0) {
-        std::cerr << "Could not retrieve stream info from file: " << filename << std::endl;
-        avformat_close_input(&formatContext);
-        return;
-    }
-
-    // 打印视频文件信息
-    av_dump_format(formatContext, 0, filename.c_str(), 0);
-
-    // 清理
-    avformat_close_input(&formatContext);
-    avformat_free_context(formatContext);
-    avformat_network_deinit();
-}
+#include "VideoState.h"
+#include "demuxer.h"
+#include "AudioDecoder.h"
+#include "VideoDecoder.h"
+#include "AudioRender.h"
+//#include "VideoR"
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <video file>" << std::endl;
-        return 1;
+    avformat_network_init();
+
+    auto now = std::chrono::high_resolution_clock::now();
+
+
+    std::string filename = "./dk.wma";
+    // audio.aac
+    //std::string filename = "./audio.aac";
+
+    //std::string filename = "jay.mp3";
+    //std::string filename = "./test.mp4";
+    //FFmpegStudy::splite_audio_video(filename);
+    //if (AVFormatContext* ctx = FFmpegStudy::open_file(filename.c_str()))
+    //{
+    //    auto index = FFmpegStudy::get_audio_vedio_index(ctx);
+    //    FFmpegStudy::decode(ctx, std::get<1>(index));
+    //    FFmpegStudy::close(ctx);
+    //}
+    //FFmpegStudyEncode::encode("./333.mp4");
+    //FFmpegStudyPlayer::player(filename.c_str());
+    //FFmpegStudyPlayer::player(filename.c_str());
+    //FFmpegStudyPlayer_Audio::player(filename.c_str());
+
+
+
+    VideoState *vs = new VideoState();
+
+    Demuxer* demuxer = new Demuxer(filename.c_str());
+    demuxer->set_video_state(vs);
+
+    AudioDecoder* audio_decoder = new AudioDecoder();
+    audio_decoder->set_video_state(vs);
+
+    AudioRender* audio_render = new AudioRender();
+    audio_render->set_video_state(vs);
+
+    demuxer->start();
+    vs->start();
+    audio_decoder->start();
+    audio_render->start();
+
+    while (1)
+    {
     }
 
-    std::string filename = argv[1];
-    print_video_info(filename);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - now) << std::endl;
 
     return 0;
 }
-
