8967117042d42212ce4420286074ab1e52721856
Rust Lyu <778324184@qq.com>
2024-08-14 10:42:42 +0800

    Create GrabWindow.h

---
+++ GrabWindow.h
@@ -0,0 +1,13 @@
+#ifndef GRAB_WINDOW_H
+#define GRAB_WINDOW_H
+
+namespace FFmpegStudy_Grab {
+	class Grab{
+	public:
+	static int grab(const char* path);
+	static bool stop_;
+	};
+	int tt();
+}
+
+#endif // GRAB_WINDOW_H
